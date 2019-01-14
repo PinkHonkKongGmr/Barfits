9ccdd00ca02e3f8d41859312872d27f31c074979
PinkHonkKongGmr <39237301+PinkHonkKongGmr@users.noreply.github.com>
2019-01-14 23:01:39 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$('.wrapper').scroll(function() {
+  var edge = $('.wrapper').height() - $('img').height() + $('.wrapper').offset().top;
+  console.log(edge);
+  console.log($('img').offset().top);
+
+  if ($('img').offset().top == edge) {
+
+    $('img').addClass('capture');
+    $('.question').addClass('goUp');
+    $('.menu').addClass('goUp2');
+    $('.wrapper').addClass('noScroll');
+
+    $('.question').animate({
+      opacity: 0.8
+    }, 200, "linear");
+
+  setTimeout(function () {
+    $('#disign').animate({
+      fontSize: '75px'
+    }, 200, "linear");
+  },600)
+
+  setTimeout(function () {
+    $('#safety').animate({
+      fontSize: '75px'
+    }, 400, "linear");
+  },700)
+    setTimeout(function () {
+    $('#price').animate({
+      fontSize: '75px'
+    }, 600, "linear");
+  },800)
+    setTimeout(function () {
+    $('#functional').animate({
+      fontSize: '75px'
+    }, 800, "linear");
+  },900)
+  }
+});
