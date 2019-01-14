83033ff2922e48fba68fccd6b33ff1d02029985f
PinkHonkKongGmr <39237301+PinkHonkKongGmr@users.noreply.github.com>
2019-01-14 23:04:01 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -14,26 +14,26 @@ $('.wrapper').scroll(function() {
       opacity: 0.8
     }, 200, "linear");
 
-  setTimeout(function () {
-    $('#disign').animate({
-      fontSize: '75px'
-    }, 200, "linear");
-  },600)
+    setTimeout(function() {
+      $('#disign').animate({
+        fontSize: '75px'
+      }, 200, "linear");
+    }, 600)
 
-  setTimeout(function () {
-    $('#safety').animate({
-      fontSize: '75px'
-    }, 400, "linear");
-  },700)
-    setTimeout(function () {
-    $('#price').animate({
-      fontSize: '75px'
-    }, 600, "linear");
-  },800)
-    setTimeout(function () {
-    $('#functional').animate({
-      fontSize: '75px'
-    }, 800, "linear");
-  },900)
+    setTimeout(function() {
+      $('#safety').animate({
+        fontSize: '75px'
+      }, 400, "linear");
+    }, 700)
+    setTimeout(function() {
+      $('#price').animate({
+        fontSize: '75px'
+      }, 600, "linear");
+    }, 800)
+    setTimeout(function() {
+      $('#functional').animate({
+        fontSize: '75px'
+      }, 800, "linear");
+    }, 900)
   }
 });
